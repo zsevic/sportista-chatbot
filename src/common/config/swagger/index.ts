3bcd51bb30771c41554b929ca7f32991bb59b10f
zsevic <zsevic95@gmail.com>
2020-01-27 00:22:30 +0100

    Move Swagger configuration into separate directory

---
+++ src/common/config/swagger/index.ts
@@ -0,0 +1,15 @@
+import { INestApplication } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { SWAGGER_API_NAME, SWAGGER_API_DESCRIPTION,
+  SWAGGER_API_CURRENT_VERSION, SWAGGER_API_ROOT } from './constants';
+
+export const setupSwagger = (app: INestApplication) => {
+  const options = new DocumentBuilder()
+  .setTitle(SWAGGER_API_NAME)
+  .setDescription(SWAGGER_API_DESCRIPTION)
+  .setVersion(SWAGGER_API_CURRENT_VERSION)
+  .build();
+  const document = SwaggerModule.createDocument(app, options);
+
+  SwaggerModule.setup(SWAGGER_API_ROOT, app, document);
+};
--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from 'modules/app/app.module';
+import { setupSwagger } from 'common/config/swagger';
 
 async function bootstrap() {
   const appOptions = { cors: true };
   const app = await NestFactory.create(AppModule, appOptions);
   app.useGlobalPipes(new ValidationPipe());
-
-  const options = new DocumentBuilder()
-    .setTitle('NestJS starter')
-    .setDescription('NestJS starter API documentation')
-    .setVersion('0.0.1')
-    .addTag('auth')
-    .build();
-  const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup('api-docs', app, document);
+  setupSwagger(app);
 
   await app.listen(process.env.PORT || 3000);
 }
+
 bootstrap();
