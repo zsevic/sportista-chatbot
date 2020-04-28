7c5d15390ef544ea53030d942f3365487bd97883
zsevic <zsevic95@gmail.com>
2020-04-28 02:15:01 +0200

    Improve code

--- src/modules/auth/auth.constants.ts
+++ src/modules/auth/auth.constants.ts
@@ -1,7 +1,9 @@
-export const COOKIE_OPTIONS = {
+import { CookieOptions } from 'express';
+
+export const COOKIE_OPTIONS: CookieOptions = {
   httpOnly: true,
   // secure: true,
-  sameSite: 'Lax',
+  sameSite: 'lax',
 };
 
 export const JWT_COOKIE_NAME = 'jwt';
