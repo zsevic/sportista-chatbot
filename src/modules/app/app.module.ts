08b431dcb2185298c044a94dafb8278f5cb4fbb1
zsevic <zsevic95@gmail.com>
2020-05-03 00:06:31 +0200

    Improve code

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -1,4 +1,4 @@
-import { Module, Logger } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { ConfigService, ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import config from 'common/config';
