991f7b50564322778cacec060922bef3a602ec8a
zsevic <zsevic95@gmail.com>
2020-05-28 03:00:29 +0200

    Improve refresh token implementation

--- src/modules/auth/guards/auth.guard.ts
+++ src/modules/auth/guards/auth.guard.ts
@@ -22,41 +22,44 @@ export class CustomAuthGuard extends AuthGuard('jwt') {
   }
 
   handleRequest(_, user, info: Error, context: ExecutionContext) {
-    if (info?.name === 'TokenExpiredError') {
-      const { cookies } = context.switchToHttp().getRequest();
-      const refreshToken = cookies[REFRESH_TOKEN_COOKIE_NAME];
-      if (!refreshToken) throw new UnauthorizedException(info.message);
-      this.authService.verifyToken(refreshToken);
+    const response = context.switchToHttp().getResponse();
+    try {
+      if (info?.name === 'TokenExpiredError') {
+        const { cookies } = context.switchToHttp().getRequest();
+        const refreshToken = cookies[REFRESH_TOKEN_COOKIE_NAME];
+        if (!refreshToken) throw new UnauthorizedException(info.message);
+        this.authService.verifyToken(refreshToken);
 
-      return this.userService
-        .getByRefreshToken(refreshToken)
-        .then(user => {
-          const { accessToken, refreshToken } = this.authService.createTokens(
-            user.id,
-          );
-          return this.userService
-            .updateRefreshToken(user.id, refreshToken)
-            .then(() => ({ accessToken, refreshToken, user }));
-        })
-        .then(({ accessToken, refreshToken, user }) => {
-          const response = context.switchToHttp().getResponse();
-          response.cookie(
-            ACCESS_TOKEN_COOKIE_NAME,
-            accessToken,
-            COOKIE_OPTIONS,
-          );
-          response.cookie(
-            REFRESH_TOKEN_COOKIE_NAME,
-            refreshToken,
-            COOKIE_OPTIONS,
-          );
+        return this.userService
+          .getByRefreshToken(refreshToken)
+          .then(user => {
+            const { accessToken, refreshToken } = this.authService.createTokens(
+              user.id,
+            );
+            return this.userService
+              .updateRefreshToken(user.id, refreshToken)
+              .then(() => ({ accessToken, refreshToken, user }));
+          })
+          .then(({ accessToken, refreshToken, user }) => {
+            response.cookie(
+              ACCESS_TOKEN_COOKIE_NAME,
+              accessToken,
+              COOKIE_OPTIONS,
+            );
+            response.cookie(
+              REFRESH_TOKEN_COOKIE_NAME,
+              refreshToken,
+              COOKIE_OPTIONS,
+            );
 
-          return user;
-        })
-        .catch((err: Error) => {
-          throw new UnauthorizedException(err.message);
-        });
+            return user;
+          });
+      }
+      return user;
+    } catch (err) {
+      response.clearCookie(ACCESS_TOKEN_COOKIE_NAME, COOKIE_OPTIONS);
+      response.clearCookie(REFRESH_TOKEN_COOKIE_NAME, COOKIE_OPTIONS);
+      throw new UnauthorizedException(err.message);
     }
-    return user;
   }
 }
