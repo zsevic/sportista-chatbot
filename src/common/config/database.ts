ed0bdab0e3a9aaf27fadfdbebf048964e4b13ee5
zsevic <zsevic95@gmail.com>
2020-05-31 19:45:24 +0200

    Improve database config

--- src/common/config/database.ts
+++ src/common/config/database.ts
@@ -1,12 +1,36 @@
+import * as path from 'path';
 import { registerAs } from '@nestjs/config';
 
-export default registerAs('database', () => ({
-  database: 'database.sqlite',
-  entities: ['dist/**/**.entity{.ts,.js}'],
+const options = {
+  entities: [path.join(__dirname, '/../../../**/*.entity.{js,ts}')],
   keepConnectionAlive: true,
   logging: false,
   migrations: ['dist/database/migrations/*{.ts,.js}'],
   migrationsTableName: 'migrations',
   synchronize: false,
-  type: 'sqlite',
-}));
+};
+
+export default registerAs('database', () => {
+  const environmentsConfig = {
+    production: {
+      ...options,
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+    },
+    test: {
+      ...options,
+      database: 'test.sqlite',
+      dropSchema: true,
+      synchronize: true,
+      type: 'sqlite',
+    },
+    development: {
+      ...options,
+      database: 'database.sqlite',
+      type: 'sqlite',
+    },
+  };
+  const currentEnvironment = process.env.NODE_ENV || 'development';
+
+  return environmentsConfig[currentEnvironment];
+});
