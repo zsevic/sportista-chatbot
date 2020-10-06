f33ae85ad6d8bea305fcbb5010aa0fb5a43216ad
zsevic <zsevic95@gmail.com>
2020-10-06 17:54:22 +0200

    Set up Heroku config

--- pm2.config.js
+++ pm2.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'app',
-      script: 'dist/src/gateways/database/connection.js',
+      script: 'dist/src/main.js',
       instances: 'max',
       env: {
         NODE_ENV: 'development',
