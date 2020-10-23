d46fcfc71e906b4df16582f6092e3fb319b16550
zsevic <zsevic95@gmail.com>
2020-10-23 21:26:36 +0200

    Configure NewRelic

---
+++ newrelic.js
@@ -0,0 +1,42 @@
+const Package = require('./package');
+
+exports.config = {
+  app_name: [process.env.NEW_RELIC_APP_NAME || Package.name],
+  agent_enabled: true,
+  high_security: false,
+  labels: {
+    Type: 'Service',
+  },
+  logging: {
+    level: 'info',
+    filepath: 'stdout',
+  },
+  allow_all_headers: true,
+  attributes: {
+    exclude: [
+      'request.headers.cookie',
+      'request.headers.authorization',
+      'request.headers.proxyAuthorization',
+      'request.headers.setCookie*',
+      'request.headers.x*',
+      'response.headers.cookie',
+      'response.headers.authorization',
+      'response.headers.proxyAuthorization',
+      'response.headers.setCookie*',
+      'response.headers.x*',
+    ],
+  },
+  distributed_tracing: {
+    enabled: true,
+  },
+  error_collector: {
+    enabled: true,
+    ignore_status_codes: [404],
+  },
+  transaction_tracer: {
+    enabled: true,
+    explain_threshold: 500,
+    record_sql: 'obfuscated',
+    hide_internals: true,
+  },
+};
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import 'newrelic';
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
