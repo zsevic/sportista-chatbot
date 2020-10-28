b80408aaedb50eb9e61af3f15d865a1768237132
zsevic <zsevic95@gmail.com>
2020-10-28 19:10:13 +0100

    Improve image ratio

--- src/modules/bots/messenger-bot/messenger-bot.utils.ts
+++ src/modules/bots/messenger-bot/messenger-bot.utils.ts
@@ -1,5 +1,5 @@
 export const getImageUrl = (activityType: string): string =>
-  `https://loremflickr.com/320/240/${activityType}`;
+  `https://loremflickr.com/460/240/${activityType}`;
 
 export const getLocationUrl = (latitude: number, longitude: number): string =>
   `http://www.google.com/maps/place/${latitude},${longitude}`;
