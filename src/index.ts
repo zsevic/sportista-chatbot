a9ba8aecbb8fdb44d8c0b228c6dcec97a9b2cf45
zsevic <zsevic95@gmail.com>
2020-11-14 23:57:46 +0100

    Refactor app creation

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,42 @@
-import { INestApplicationContext } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import {
+  ExpressAdapter,
+  NestExpressApplication,
+} from '@nestjs/platform-express';
+import bodyParser from 'body-parser';
+import express from 'express';
+import { WinstonModule } from 'nest-winston';
+import { format, transports } from 'winston';
 import { AppModule } from 'modules/app/app.module';
 import { BotsModule } from 'modules/bots/bots.module';
 import { BotsService } from 'modules/bots/bots.service';
 
-const appContext = (function () {
-  let _instance;
+export const application = (function () {
+  let _instance: NestExpressApplication;
   return {
-    get: async (): Promise<INestApplicationContext> => {
+    get: async (): Promise<NestExpressApplication> => {
       if (!_instance) {
-        _instance = await NestFactory.createApplicationContext(AppModule);
+        const server = express();
+        const verify = (req, _, buf): void => {
+          req.rawBody = buf.toString();
+        };
+        server.use(bodyParser.json({ verify }));
+        server.use(bodyParser.urlencoded({ extended: false, verify }));
+
+        _instance = await NestFactory.create<NestExpressApplication>(
+          AppModule,
+          new ExpressAdapter(server),
+          {
+            logger: WinstonModule.createLogger({
+              format: format.combine(format.timestamp(), format.json()),
+              transports: [
+                new transports.Console({
+                  level: process.env.LOG_LEVEL || 'info',
+                }),
+              ],
+            }),
+          },
+        );
       }
       return _instance;
     },
@@ -17,7 +44,7 @@ const appContext = (function () {
 })();
 
 export default async function App() {
-  const app = await appContext.get();
+  const app = await application.get();
   const chatbotService = app
     .select(BotsModule)
     .get(BotsService, { strict: true });
--- src/main.ts
+++ src/main.ts
@@ -1,52 +1,24 @@
 import path from 'path';
 import { Logger } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
-import {
-  ExpressAdapter,
-  NestExpressApplication,
-} from '@nestjs/platform-express';
 import * as Sentry from '@sentry/node';
-import bodyParser from 'body-parser';
 import { bottender } from 'bottender';
 import compression from 'compression';
 import cookieParser from 'cookie-parser';
-import express from 'express';
 import helmet from 'helmet';
-import { WinstonModule } from 'nest-winston';
 import throng from 'throng';
-import { format, transports } from 'winston';
 import { setupApiDocs } from 'common/config/api-docs';
 import { AllExceptionsFilter } from 'common/filters';
 import { loggerMiddleware, sslRedirect } from 'common/middlewares';
 import { CustomValidationPipe } from 'common/pipes';
 import { isEnv } from 'common/utils';
 import { AppModule } from 'modules/app/app.module';
+import { application } from './index';
 
 const bottenderApp = bottender({ dev: !isEnv('production') });
 export const handle = bottenderApp.getRequestHandler();
 
 async function bootstrap(): Promise<void> {
-  const server = express();
-  const verify = (req, _, buf): void => {
-    req.rawBody = buf.toString();
-  };
-  server.use(bodyParser.json({ verify }));
-  server.use(bodyParser.urlencoded({ extended: false, verify }));
-
-  const app = await NestFactory.create<NestExpressApplication>(
-    AppModule,
-    new ExpressAdapter(server),
-    {
-      logger: WinstonModule.createLogger({
-        format: format.combine(format.timestamp(), format.json()),
-        transports: [
-          new transports.Console({
-            level: process.env.LOG_LEVEL || 'info',
-          }),
-        ],
-      }),
-    },
-  );
+  const app = await application.get();
   const logger = new Logger(bootstrap.name);
   const configService = app.get('configService');
 
